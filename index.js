af18055bf09b1288239b88583b6a1957c3a3c545
opicivezio <dptuscps@naver.com>
2020-07-31 18:46:05 +0900

    1.2

--- index.js
+++ index.js
@@ -111,6 +111,21 @@ client.on('message', (message) => {
       .setFooter('Embed Made by PLAYER', img2)
 
     message.channel.send(embed)
+  } else if(message.content == '!서버제작') {
+    let img3 = 'https://postfiles.pstatic.net/MjAyMDA3MjlfMTE3/MDAxNTk1OTg3OTI4MjM4.BUW6UH-rR415TVO92s4XbgIcUgu31f8HFwcBJR7s15gg.UKaWE8P95zcWGjtZsn4Ef62oJs0kOLu7IoFiWihtWP4g.PNG.battleground_bloger/PLAYER.png?type=w773';
+    let img4 = 'https://postfiles.pstatic.net/MjAyMDA3MjhfMjYz/MDAxNTk1OTQxNTExNzEx.L7PmxCPS8OyHRXR3ApuJoTcW1OXAkfFyEkXTqMkeaGog.hpsnSNltgPtcr43dph0VDSXHYaHGmeKcsagx2rzKCmQg.JPEG.battleground_bloger/KakaoTalk_20200712_234541407.jpg?type=w773';
+    let embed = new Discord.RichEmbed()
+      .setTitle('서버제작자')
+      .setAuthor('Ashaonxen Bot', img4)
+      .setThumbnail(img4)
+      .addBlankField()
+      .addField('서버제작자', '`PLAYER#9642`')
+      .addField('제작에 도움을 준 사람', '`MowY#8644`')
+      .addBlankField()
+      .setTimestamp()
+      .setFooter('Embed Made by PLYER', img3)
+
+    message.channal.send(embed)
   } else if(message.content == '!에션첸 help') {
     let helpImg = 'https://postfiles.pstatic.net/MjAyMDA3MjhfMjYz/MDAxNTk1OTQxNTExNzEx.L7PmxCPS8OyHRXR3ApuJoTcW1OXAkfFyEkXTqMkeaGog.hpsnSNltgPtcr43dph0VDSXHYaHGmeKcsagx2rzKCmQg.JPEG.battleground_bloger/KakaoTalk_20200712_234541407.jpg?type=w773';
     let commandList = [
